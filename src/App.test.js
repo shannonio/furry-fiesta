705c5a49abd7e74a09398e229bb192a7e03750c2
Shannon Bertucci <shannon.bertucci@gmail.com>
2019-12-03 23:06:58 -0800

    Update App.test with Snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
 import App from './App';
+
+const mockStore = configureStore([]);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = mockStore({
+    user: {},
+  });
+
+  const component = ReactDOM.render((
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ), div);
+
+  expect(component).toMatchSnapshot();
+
   ReactDOM.unmountComponentAtNode(div);
 });
